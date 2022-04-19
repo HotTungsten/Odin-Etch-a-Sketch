8a3cb5fc86b5306402e18b272a49f8be2e04b3c4
unknown <hottungsten@gmail.com>
2022-04-19 13:11:04 -0700

    dimension prompt

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const SIZE = 16*16 + 17;
 
-const container = document.querySelector('.container');
+const gridBox = document.querySelector('.gridBox');
 
 //16x16 grid of boxes
 for(let i = 0; i < SIZE; i++){
@@ -12,17 +12,25 @@ for(let i = 0; i < SIZE; i++){
     else {
         div.classList.add('default');
     }
-    container.appendChild(div);
+    gridBox.appendChild(div);
 }
 
 //colors grid when moused over
 const sketchBox = document.querySelectorAll('.default');
-sketchBox.forEach(sketchBox => sketchBox.addEventListener('mouseover', function(e) {
+sketchBox.forEach(sketchBox => sketchBox.addEventListener('mouseover', function() {
     this.classList.add('mousedOver');
 }));
 
-//removes color from all the boxes
+//removes color from all the boxes, allows new grid to be created
 const resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click', function(){
     sketchBox.forEach(sketchBox => sketchBox.classList.remove("mousedOver"));
+
+    //initialized outside of do-while -- scope
+    let gridPrompt;
+    do {
+        gridPrompt = parseInt(prompt("Please enter new grid dimensions (max 100): "), 10);
+    }while(isNaN(gridPrompt) || gridPrompt > 100);
+
+
 });
