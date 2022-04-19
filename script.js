e1aed0665411d52ed265202cd2fd8aa3618fab1b
unknown <hottungsten@gmail.com>
2022-04-19 14:17:46 -0700

    allowed reset and grid recreation

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-const SIZE = 16*16 + 17;
+const SIZE = 16
 
-const gridBox = document.querySelector('.gridBox');
-
-//16x16 grid of boxes
-for(let i = 0; i < SIZE; i++){
-    const div = document.createElement('div');
-    const hr = document.createElement('hr');
-    if(i % 17 == 0){
-        div.classList.add('break');
+function createGrid(gridSize){
+    //specified grid of boxes
+    let totalGrid = gridSize * gridSize + (gridSize + 1);
+    const gridBox = document.querySelector('.gridBox');
+    for(let i = 0; i < totalGrid; i++){
+        const div = document.createElement('div');
+        const hr = document.createElement('hr');
+        if(i % (gridSize+1) == 0){
+            div.classList.add('break');
+        }
+        else {
+            div.classList.add('default');
+            div.style.height = 300/gridSize + 'px';
+            div.style.width = 300/gridSize + 'px';
+        }
+        gridBox.appendChild(div);
     }
-    else {
-        div.classList.add('default');
-    }
-    gridBox.appendChild(div);
 }
 
+createGrid(SIZE);
+
 //colors grid when moused over
-const sketchBox = document.querySelectorAll('.default');
+let sketchBox = document.querySelectorAll('.default');
+let breaks = document.querySelectorAll('.break');
 sketchBox.forEach(sketchBox => sketchBox.addEventListener('mouseover', function() {
     this.classList.add('mousedOver');
 }));
@@ -24,13 +31,19 @@ sketchBox.forEach(sketchBox => sketchBox.addEventListener('mouseover', function(
 //removes color from all the boxes, allows new grid to be created
 const resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click', function(){
-    sketchBox.forEach(sketchBox => sketchBox.classList.remove("mousedOver"));
-
+    sketchBox.forEach(sketchBox => sketchBox.remove("mousedOver", "default"));
+    breaks.forEach(breaks => breaks.remove('break'));
     //initialized outside of do-while -- scope
     let gridPrompt;
     do {
         gridPrompt = parseInt(prompt("Please enter new grid dimensions (max 100): "), 10);
     }while(isNaN(gridPrompt) || gridPrompt > 100);
 
-
+    //recreates grid with sketching enabled
+    createGrid(gridPrompt);
+    sketchBox = document.querySelectorAll('.default');
+    sketchBox.forEach(sketchBox => sketchBox.addEventListener('mouseover', function() {
+        this.classList.add('mousedOver');
+    }));
+    
 });
